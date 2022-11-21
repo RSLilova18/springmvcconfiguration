5f5a1327b367e646c0cd41fdc5a0246653b06dda
RSLilova18 <RSLilova18@codingburgas.bg>
2022-11-21 12:49:38 +0200

    Initial commit

---
+++ src/main/java/com/example/springmvcconfiguration/SpringmvcconfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.example.springmvcconfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmvcconfigurationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringmvcconfigurationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springmvcconfiguration/SpringmvcconfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springmvcconfiguration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmvcconfigurationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
